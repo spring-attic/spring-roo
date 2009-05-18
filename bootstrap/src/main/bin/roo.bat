513ef42339f569cf41ec080957a25ba6613a5d41
Ben Alex <balex@vmware.com>
2009-05-18 04:30:07 +0000

    ROO-3: Enhancements to launch scripts to eliminate ROO_HOME variable requirement, and also ensure correct operation with spaces in path name. Tested successfully under Ubuntu 9.04 and Windows 2000 Server. Apple testing to follow.

--- bootstrap/src/main/bin/roo.bat
+++ bootstrap/src/main/bin/roo.bat
@@ -1,12 +1,14 @@
 @echo off
 
-if "%ROO_HOME%"=="" goto instructions
+setlocal
+
+rem %~dp0 is location of current script under NT
+set _REALPATH=%~dp0
+
+cd %_REALPATH%
+cd ..
+set ROO_HOME="%cd%"
+
+rem echo Resolved ROO_HOME: %ROO_HOME%
 
 java -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
-goto end
-
-:instructions
-echo "ERROR: ROO_HOME environment variable is not set"
-
-:end
-
--- bootstrap/src/main/bin/roo.sh
+++ bootstrap/src/main/bin/roo.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if [ -z "$ROO_HOME" ]; then
-    echo "ERROR: ROO_HOME environment variable is not set"
-else
-    java -Djava.ext.dirs=$ROO_HOME/lib:$ROO_HOME/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
-fi
+PRG="$0"
+SYM_LINK=`readlink "$PRG"`
+BIN_HOME=`dirname "$SYM_LINK"`
+ROO_HOME=`dirname "$BIN_HOME"`
+
+# echo Resolved SYM_LINK: $SYM_LINK
+# echo Resolved BIN_HOME: $BIN_HOME
+# echo Resolved ROO_HOME: $ROO_HOME
+
+java -Djava.ext.dirs="$ROO_HOME/lib:$ROO_HOME/dist" org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
