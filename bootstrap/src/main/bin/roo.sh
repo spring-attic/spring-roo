554c39e0ff74246f2417437798006cb644d364d6
Ben Alex <balex@vmware.com>
2009-05-21 03:56:25 +0000

    ROO-3: Enhancements to launch scripts to eliminate ROO_HOME variable requirement
    The previous version of this file was incompatible with Apple machines, despite being simpler. Reverted to the more complex version.

--- bootstrap/src/main/bin/roo.sh
+++ bootstrap/src/main/bin/roo.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 PRG="$0"
-SYM_LINK=`readlink "$PRG"`
-BIN_HOME=`dirname "$SYM_LINK"`
-ROO_HOME=`dirname "$BIN_HOME"`
 
-# echo Resolved SYM_LINK: $SYM_LINK
-# echo Resolved BIN_HOME: $BIN_HOME
-# echo Resolved ROO_HOME: $ROO_HOME
+while [ -h "$PRG" ]; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`/"$link"
+    fi
+done
+ROO_HOME=`dirname "$PRG"`
+
+#Absolute path
+ROO_HOME=`cd "$ROO_HOME/.." ; pwd`
+
+echo Resolved ROO_HOME: $ROO_HOME
 
 java -Djava.ext.dirs="$ROO_HOME/lib:$ROO_HOME/dist" org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
