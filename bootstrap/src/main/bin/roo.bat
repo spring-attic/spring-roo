86db2dcde7fcc5107f799fd18f6757b0c3af3f2e
Ben Alex <balex@vmware.com>
2009-05-22 07:49:42 +0000

    ROO-3: Enhancements to launch scripts to eliminate ROO_HOME variable requirement

--- bootstrap/src/main/bin/roo.bat
+++ bootstrap/src/main/bin/roo.bat
@@ -2,6 +2,8 @@
 
 setlocal
 
+set PROJECT_DIRECTORY="%cd%"
+
 rem %~dp0 is location of current script under NT
 set _REALPATH=%~dp0
 
@@ -9,6 +11,10 @@ cd %_REALPATH%
 cd ..
 set ROO_HOME="%cd%"
 
+cd %PROJECT_DIRECTORY%
+
 rem echo Resolved ROO_HOME: %ROO_HOME%
+rem echo Resolved PROJECT_DIRECTORY %PROJECT_DIRECTORY%
+
 
 java -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
