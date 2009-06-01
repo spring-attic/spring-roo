173832284f7e215b16fb38a340b4b92fc4a73675
Ben Alex <balex@vmware.com>
2009-06-01 03:16:07 +0000

    ROO-59: Windows batch launch script fails if different drive letters used from project

--- bootstrap/src/main/bin/roo.bat
+++ bootstrap/src/main/bin/roo.bat
@@ -1,20 +1,7 @@
 @echo off
 
-setlocal
-
-set PROJECT_DIRECTORY="%cd%"
-
-rem %~dp0 is location of current script under NT
-set _REALPATH=%~dp0
-
-cd %_REALPATH%
-cd ..
-set ROO_HOME="%cd%"
-
-cd %PROJECT_DIRECTORY%
+for %%? in ("%~dp0..") do set ROO_HOME=%%~f?
 
 rem echo Resolved ROO_HOME: %ROO_HOME%
-rem echo Resolved PROJECT_DIRECTORY %PROJECT_DIRECTORY%
-
 
 java -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
