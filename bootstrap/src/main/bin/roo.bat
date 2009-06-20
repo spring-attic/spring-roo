af056484ac5bffbe5a28837fa6a2bf105c0a601e
Ben Alex <balex@vmware.com>
2009-06-20 06:23:16 +0000

    ROO-110: Stop beep noise on Windows machines via Roo shell

--- bootstrap/src/main/bin/roo.bat
+++ bootstrap/src/main/bin/roo.bat
@@ -4,4 +4,4 @@ for %%? in ("%~dp0..") do set ROO_HOME=%%~f?
 
 rem echo Resolved ROO_HOME: %ROO_HOME%
 
-java -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
+java -Djline.nobell=true -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml"
