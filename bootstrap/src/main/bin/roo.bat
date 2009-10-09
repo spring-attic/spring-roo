dda60e3e7b544fc3f5afd17824f68ea7977209f3
Ben Alex <balex@vmware.com>
2009-10-09 00:17:34 +0000

    ROO-281: roo.bat fails if installed in a directory with space in it's path

--- bootstrap/src/main/bin/roo.bat
+++ bootstrap/src/main/bin/roo.bat
@@ -2,6 +2,6 @@
 
 for %%? in ("%~dp0..") do set ROO_HOME=%%~f?
 
-rem echo Resolved ROO_HOME: %ROO_HOME%
+rem echo Resolved ROO_HOME: "%ROO_HOME%"
 
-java -Djline.nobell=true -Djava.ext.dirs=%ROO_HOME%/lib;%ROO_HOME%/dist org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml" %*
+java -Djline.nobell=true -Djava.ext.dirs="%ROO_HOME%/lib;%ROO_HOME%/dist" org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml" %*
