948a9bad2fa89965f8447d50377afda57167e8d3
Joris Kuipers <jkuipers@vmware.com>
2009-11-15 07:24:16 +0000

    ROO-381: Errors in roo-dev.bat - fixes working with addons

--- bootstrap/roo-dev.bat
+++ bootstrap/roo-dev.bat
@@ -11,12 +11,12 @@ rem echo Using Roo classpath file [%ROO_CLASSPATH_FILE%]
 rem Will be only one line, but the following obvious form doesn't work
 rem set /p ROO_CP=< %ROO_CLASSPATH_FILE%
 
-for /f "tokens=* delims= " %%a in (%ROO_CLASSPATH_FILE%) do (
+for /f "usebackq tokens=* delims= " %%a in (%ROO_CLASSPATH_FILE%) do (
 	set ROO_CP=%%a
 )
 
-if not "%ROO_ADDON_CLASSPATH_FILE%" == "" goto run
-for /f "tokens=* delims= " %%a in (%ROO_ADDON_CLASSPATH_FILE%) do (
+if "%ROO_ADDON_CLASSPATH_FILE%" == "" goto run
+for /f "usebackq tokens=* delims= " %%a in (%ROO_ADDON_CLASSPATH_FILE%) do (
 	set EXTENDED_CP=%%a
 )
 
@@ -30,6 +30,6 @@ set ROO_CP="%ROO_CP%"
 rem echo Using Roo classpath [%ROO_CP%]
 
 :launch
-java -DdevelopmentMode=true %ROO_OPTS% -Droo.home="%USERPROFILE%/roo-dev" -cp %ROO_CP% -Djava.ext.dirs="%USERPROFILE%/roo-dev/work;%JAVA_HOME%/jre/lib/ext" org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml" %*
+java -Djline.nobell=true -DdevelopmentMode=true %ROO_OPTS% -Droo.home="%USERPROFILE%\roo-dev" -cp %ROO_CP% -Djava.ext.dirs="%USERPROFILE%\roo-dev\work;%JAVA_HOME%\jre\lib\ext" org.springframework.roo.bootstrap.Bootstrap "classpath:/roo-bootstrap.xml" %*
 
 if "%errorlevel%" == "100" goto launch
