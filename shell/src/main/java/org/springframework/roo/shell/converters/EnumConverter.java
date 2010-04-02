d3a8c94937c66efa7250cbb42f466b2f0503b125
Alan Stewart <stewarta@vmware.com>
2010-04-02 08:03:08 +0000

    OPEN - issue ROO-505: Need support for @EmbeddedId - a way to specify this in @RooEntity http://jira.springframework.org/browse/ROO-505 Added check for lowercase-entered text for tab completion of (uppercase) Enum attributes

--- shell/src/main/java/org/springframework/roo/shell/converters/EnumConverter.java
+++ shell/src/main/java/org/springframework/roo/shell/converters/EnumConverter.java
@@ -11,6 +11,7 @@ import org.springframework.roo.shell.MethodTarget;
  * {@link Converter} for {@link Enum}.
  *
  * @author Ben Alex
+ * @author Alan Stewart
  * @since 1.0
  *
  */
@@ -29,7 +30,7 @@ public class EnumConverter implements Converter {
 		Class<Enum> enumClass = (Class<Enum>) requiredType;
 		for (Enum enumValue : enumClass.getEnumConstants()) {
 			String candidate = enumValue.name();
-			if ("".equals(existingData) || candidate.startsWith(existingData) || existingData.startsWith(candidate)) {
+			if ("".equals(existingData) || candidate.startsWith(existingData) || existingData.startsWith(candidate) || candidate.toUpperCase().startsWith(existingData.toUpperCase()) || existingData.toUpperCase().startsWith(candidate.toUpperCase())) {
 				completions.add(enumValue.name());
 			}
 		}
