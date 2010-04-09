81dfde8eedbee8336c6e37f7eadf1226115688a4
Alan Stewart <stewarta@vmware.com>
2010-04-09 01:29:40 +0000

    OPEN - issue ROO-505: Need support for @EmbeddedId - a way to specify this in @RooEntity http://jira.springframework.org/browse/ROO-505

---
+++ addon-serializable/src/main/java/org/springframework/roo/addon/serializable/SerializableMetadataProviderImpl.java
@@ -0,0 +1,49 @@
+package org.springframework.roo.addon.serializable;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.component.ComponentContext;
+import org.springframework.roo.classpath.PhysicalTypeIdentifier;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.itd.AbstractItdMetadataProvider;
+import org.springframework.roo.classpath.itd.ItdTypeDetailsProvidingMetadataItem;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.Path;
+
+/**
+ * Provides {@link SerializableMetadata}.
+ * 
+ * @author Alan Stewart
+ * @since 1.1
+ */
+@Component
+@Service
+public final class SerializableMetadataProviderImpl extends AbstractItdMetadataProvider implements SerializableMetadataProvider {
+
+	protected void activate(ComponentContext context) {
+		metadataDependencyRegistry.registerDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
+		addMetadataTrigger(new JavaType(RooSerializable.class.getName()));
+	}
+	
+	protected ItdTypeDetailsProvidingMetadataItem getMetadata(String metadataIdentificationString, JavaType aspectName, PhysicalTypeMetadata governorPhysicalTypeMetadata, String itdFilename) {
+		return new SerializableMetadata(metadataIdentificationString, aspectName, governorPhysicalTypeMetadata);
+	}
+
+	public String getItdUniquenessFilenameSuffix() {
+		return "Serializable";
+	}
+
+	protected String getGovernorPhysicalTypeIdentifier(String metadataIdentificationString) {
+		JavaType javaType = SerializableMetadata.getJavaType(metadataIdentificationString);
+		Path path = SerializableMetadata.getPath(metadataIdentificationString);
+		return PhysicalTypeIdentifier.createIdentifier(javaType, path);
+	}
+
+	protected String createLocalIdentifier(JavaType javaType, Path path) {
+		return SerializableMetadata.createIdentifier(javaType, path);
+	}
+
+	public String getProvidesType() {
+		return SerializableMetadata.getMetadataIdentiferType();
+	}
+}
