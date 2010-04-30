405a5e24194c1531772a2a9dfa0297c20323e59c
Alan Stewart <stewarta@vmware.com>
2010-04-30 12:24:51 +1000

    ROO-760 Google Web Toolkit (GWT) Integration Fixed bug in cleanup() method

--- addon-jpa/src/main/java/org/springframework/roo/addon/jpa/JdbcDatabase.java
+++ addon-jpa/src/main/java/org/springframework/roo/addon/jpa/JdbcDatabase.java
@@ -1,45 +1,55 @@
 package org.springframework.roo.addon.jpa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.roo.support.style.ToStringCreator;
 
 /**
  * Provides information related to JDBC database configuration.
- *
+ * 
  * @author Stefan Schmidt
  * @author Alan Stewart
  * @since 1.0
  */
-public enum JdbcDatabase  {
-	H2_IN_MEMORY("org.h2.Driver", "jdbc:h2:mem:TO_BE_CHANGED_BY_ADDON;DB_CLOSE_DELAY=-1"),
-	HYPERSONIC_IN_MEMORY("org.hsqldb.jdbcDriver", "jdbc:hsqldb:mem:TO_BE_CHANGED_BY_ADDON"),
-	HYPERSONIC_PERSISTENT("org.hsqldb.jdbcDriver", "jdbc:hsqldb:${user.home}/TO_BE_CHANGED_BY_ADDON;shutdown=true"),
-	POSTGRES("org.postgresql.Driver", "jdbc:postgresql://localhost:5432"),
-	MYSQL("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306"),
-	ORACLE("oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@localhost:1521"),
-	SYBASE("com.sybase.jdbc2.jdbc.SybDriver", "jdbc:sybase:Tds:localhost:4100"),
-	MSSQL("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:microsoft:sqlserver://localhost:1433"),
-	DB2("com.ibm.db2.jcc.DB2Driver", "jdbc:db2://localhost:50000"),
-	GOOGLE_APP_ENGINE("", "appengine");
+public enum JdbcDatabase {
+	H2_IN_MEMORY("H2", "org.h2.Driver", "jdbc:h2:mem:TO_BE_CHANGED_BY_ADDON;DB_CLOSE_DELAY=-1"), 
+	HYPERSONIC_IN_MEMORY("HYPERSONIC", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:mem:TO_BE_CHANGED_BY_ADDON"), 
+	HYPERSONIC_PERSISTENT("HYPERSONIC", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:${user.home}/TO_BE_CHANGED_BY_ADDON;shutdown=true"), 
+	POSTGRES("POSTGRES", "org.postgresql.Driver", "jdbc:postgresql://localhost:5432"), 
+	MYSQL("MYSQL", "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306"), 
+	ORACLE("ORACLE", "oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@localhost:1521"), 
+	SYBASE("SYBASE", "com.sybase.jdbc2.jdbc.SybDriver", "jdbc:sybase:Tds:localhost:4100"), 
+	MSSQL("MSSQL", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:microsoft:sqlserver://localhost:1433"), 
+	DB2("DB2", "com.ibm.db2.jcc.DB2Driver", "jdbc:db2://localhost:50000"), 
+	GOOGLE_APP_ENGINE("GAE", "", "appengine");
 
+	private String key;
 	private String connectionString;
 	private String driverClassName;
 
-	private JdbcDatabase(String driverClassName, String connectionString) {
+	private JdbcDatabase(String key, String driverClassName, String connectionString) {
+		this.key = key;
 		this.driverClassName = driverClassName;
 		this.connectionString = connectionString;
 	}
 
+	public String getKey() {
+		return key;
+	}
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+	
 	public String getConnectionString() {
 		return connectionString;
 	}
 
-	public String getDriverClassName(){
-		return driverClassName;
-	}
-	
 	public String toString() {
 		ToStringCreator tsc = new ToStringCreator(this);
 		tsc.append("name", name());
+		tsc.append("key", key);
 		tsc.append("driver class name", driverClassName);
 		tsc.append("connection string", connectionString);
 		return tsc.toString();
