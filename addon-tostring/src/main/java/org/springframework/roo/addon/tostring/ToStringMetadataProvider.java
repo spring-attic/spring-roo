5d8c768fd0eeb5c31e605ab067dded0797f325e7
Alan Stewart <stewarta@vmware.com>
2010-08-09 15:31:33 +1000

    ROO-1191: Provide deactivate method in ToStringMetadata

--- addon-tostring/src/main/java/org/springframework/roo/addon/tostring/ToStringMetadataProvider.java
+++ addon-tostring/src/main/java/org/springframework/roo/addon/tostring/ToStringMetadataProvider.java
@@ -18,14 +18,13 @@ import org.springframework.roo.project.Path;
  * 
  * @author Ben Alex
  * @since 1.0
- *
+ * 
  */
-@Component(immediate=true)
+@Component(immediate = true)
 @Service
 public final class ToStringMetadataProvider extends AbstractItdMetadataProvider {
-
 	@Reference private BeanInfoMetadataProvider beanInfoMetadataProvider;
-	
+
 	protected void activate(ComponentContext context) {
 		metadataDependencyRegistry.registerDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
 		// Ensure we're notified of all metadata related to physical Java types, in particular their initial creation
@@ -33,26 +32,30 @@ public final class ToStringMetadataProvider extends AbstractItdMetadataProvider 
 		beanInfoMetadataProvider.addMetadataTrigger(new JavaType(RooToString.class.getName()));
 		addMetadataTrigger(new JavaType(RooToString.class.getName()));
 	}
-	
+
+	protected void deactivate(ComponentContext context) {
+		beanInfoMetadataProvider.removeMetadataTrigger(new JavaType(RooToString.class.getName()));
+	}
+
 	protected ItdTypeDetailsProvidingMetadataItem getMetadata(String metadataIdentificationString, JavaType aspectName, PhysicalTypeMetadata governorPhysicalTypeMetadata, String itdFilename) {
 		// Acquire bean info (we need getters details, specifically)
 		JavaType javaType = ToStringMetadata.getJavaType(metadataIdentificationString);
 		Path path = ToStringMetadata.getPath(metadataIdentificationString);
 		String beanInfoMetadataKey = BeanInfoMetadata.createIdentifier(javaType, path);
 
-		// We want to be notified if the getter info changes in any way 
+		// We want to be notified if the getter info changes in any way
 		metadataDependencyRegistry.registerDependency(beanInfoMetadataKey, metadataIdentificationString);
 		BeanInfoMetadata beanInfoMetadata = (BeanInfoMetadata) metadataService.get(beanInfoMetadataKey);
-		
+
 		// Abort if we don't have getter information available
 		if (beanInfoMetadata == null) {
 			return null;
 		}
-		
+
 		// Otherwise go off and create the to String metadata
 		return new ToStringMetadata(metadataIdentificationString, aspectName, governorPhysicalTypeMetadata, beanInfoMetadata);
 	}
-	
+
 	public String getItdUniquenessFilenameSuffix() {
 		return "ToString";
 	}
@@ -63,7 +66,7 @@ public final class ToStringMetadataProvider extends AbstractItdMetadataProvider 
 		String physicalTypeIdentifier = PhysicalTypeIdentifier.createIdentifier(javaType, path);
 		return physicalTypeIdentifier;
 	}
-	
+
 	protected String createLocalIdentifier(JavaType javaType, Path path) {
 		return ToStringMetadata.createIdentifier(javaType, path);
 	}
@@ -71,5 +74,5 @@ public final class ToStringMetadataProvider extends AbstractItdMetadataProvider 
 	public String getProvidesType() {
 		return ToStringMetadata.getMetadataIdentiferType();
 	}
-	
+
 }
