0ac5bee1830f8496c8f7ff93790061f1711d24e1
Alan Stewart <stewarta@vmware.com>
2010-08-23 19:26:16 +1000

    ROO-670: Support for Java DB

--- addon-jdbc/src/main/java/org/springframework/roo/addon/jdbc/util/DerbyUtils.java
+++ addon-jdbc/src/main/java/org/springframework/roo/addon/jdbc/util/DerbyUtils.java
@@ -6,9 +6,14 @@ import java.io.OutputStream;
  * Utility class to direct output of derby logging to an {@link OutputStream}, thus suppressing the creation of the derby.log file.
  * 
  * <p>
- * To take effect in Roo, add the following to the roo-dev and roo-dev.bat files:
+ * To take effect in Roo, either add the following to the roo-dev and roo-dev.bat files 
  * <br>
  * <code>-Dderby.stream.error.field=org.springframework.roo.addon.jdbc.util.DerbyUtils.DEV_NULL</code>
+ * <br>
+ * or entering,
+ * <code>export ROO_OPTS="-Dderby.stream.error.field=org.springframework.roo.addon.jdbc.util.DerbyUtils.DEV_NULL"</code>
+ * <br>
+ * on the command line before starting the Roo shell.
  * 
  * @author Alan Stewart
  * @since 1.1
