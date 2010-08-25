c0855a360f5f9e9af17bd51f9d070f13a145c3dd
Ben Alex <balex@vmware.com>
2010-08-25 14:36:21 +1000

    ROO-1116: Allow use of # for comments in scripts

--- shell/src/main/java/org/springframework/roo/shell/AbstractShell.java
+++ shell/src/main/java/org/springframework/roo/shell/AbstractShell.java
@@ -142,8 +142,8 @@ public abstract class AbstractShell extends AbstractShellStatusPublisher impleme
 			}
 			// We also support inline comments (but only at start of line, otherwise valid 
 			// command options like http://www.helloworld.com will fail as per ROO-517)
-			if (!inBlockComment && line.trim().startsWith("//")) {
-				line = line.substring(0, line.indexOf("//"));
+			if (!inBlockComment && (line.trim().startsWith("//") || line.trim().startsWith("#"))) { // # support in ROO-1116
+				line = "";
 			}
 			// convert any TAB characters to whitespace (ROO-527)
 			line = line.replace('\t', ' ');
