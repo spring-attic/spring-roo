099c0b7f950b5e888aacd8744ca81e60b1248873
Ben Alex <balex@vmware.com>
2010-12-13 20:14:02 +1100

    ROO-1816: BundleFindingUtils to offer ability to retrieve a Class instance from an unknown bundle

--- support-osgi/src/main/java/org/springframework/roo/support/osgi/BundleFindingUtils.java
+++ support-osgi/src/main/java/org/springframework/roo/support/osgi/BundleFindingUtils.java
@@ -16,7 +16,8 @@ import org.springframework.roo.support.util.Assert;
 public abstract class BundleFindingUtils {
 	
 	/**
-	 * Locates the first bundle that contains the presented type name.
+	 * Locates the first bundle that contains the presented type name and return its
+	 * bundle symbolic name. 
 	 * 
 	 * @param context that can be used to obtain bundles to search (required)
 	 * @param typeNameInExternalForm a type name (eg com.foo.Bar)
@@ -39,6 +40,38 @@ public abstract class BundleFindingUtils {
 					return bundle.getSymbolicName();
 				}
 			} catch (RuntimeException e) {
+				return null;
+			}
+		}
+		
+		return null;
+	}
+
+	/**
+	 * Locates the first bundle that contains the presented type name and return
+	 * that class.
+	 * 
+	 * @param context that can be used to obtain bundles to search (required)
+	 * @param typeNameInExternalForm a type name (eg com.foo.Bar)
+	 * @return the class, if found (or null if not found)
+	 */
+	public static Class<?> findFirstBundleWithType(BundleContext context, String typeNameInExternalForm) {
+		Assert.notNull(context, "Bundle context required to perform the search");
+		Assert.hasText(typeNameInExternalForm, "Resource name to locate is required");
+		String resourceName = "/" + typeNameInExternalForm.replace('.', '/') + ".class";
+	
+		Bundle[] bundles = context.getBundles();
+		if (bundles == null) {
+			return null;
+		}
+		
+		for (Bundle bundle : bundles) {
+			try {
+				URL url = bundle.getEntry(resourceName);
+				if (url != null) {
+					return bundle.loadClass(typeNameInExternalForm);
+				}
+			} catch (Throwable e) {
 				return null;
 			}
 		}
