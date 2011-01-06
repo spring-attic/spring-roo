e7d5c81b60232d468c0d0500fe53f114f08467a8
Stefan Schmidt <schmidts@vmware.com>
2011-01-06 11:53:06 +1100

    ROO-1920: addon 'hints' could be more informative

--- addon-roobot-client/src/main/java/org/springframework/roo/addon/roobot/client/AddOnCommands.java
+++ addon-roobot-client/src/main/java/org/springframework/roo/addon/roobot/client/AddOnCommands.java
@@ -10,7 +10,6 @@ import org.springframework.roo.shell.CliCommand;
 import org.springframework.roo.shell.CliOption;
 import org.springframework.roo.shell.CommandMarker;
 import org.springframework.roo.shell.converters.StaticFieldConverter;
-import org.springframework.roo.support.util.Assert;
 
 /**
  * Commands for this add-on.
@@ -34,20 +33,19 @@ public class AddOnCommands implements CommandMarker {
 		staticFieldConverter.remove(Rating.class);
 	}
 	
-	@CliCommand(value="addon info", help="Provide information about a specific Spring Roo Add-on")
-	public void info(@CliOption(key={"", "bundleId"}, mandatory=false, help="The bundle ID as presented via the addon list or addon search command") String bundleId,
-			@CliOption(key="bundleSymbolicName", mandatory=false, help="The bundle symbolic name for the add-on of interest") AddOnBundleSymbolicName bsn) {
-		Assert.isTrue(bsn != null || (bundleId != null && bundleId.length() > 0), "Either a --bundleId (from a search) or --bundleSymbolicName is required");
-		if (bsn != null) {
-			operations.addOnInfo(bsn);
-		} else {
-			operations.addOnInfo(bundleId);
-		}
+	@CliCommand(value="addon info id", help="Provide information about a specific Spring Roo Add-on")
+	public void infoId(@CliOption(key={"", "searchResultId"}, mandatory=true, help="The bundle ID as presented via the addon list or addon search command") String bundleId) {	
+		operations.addOnInfo(bundleId);
+	}
+	
+	@CliCommand(value="addon info bundle", help="Provide information about a specific Spring Roo Add-on")
+	public void infoBundle(@CliOption(key="bundleSymbolicName", mandatory=true, help="The bundle symbolic name for the add-on of interest") AddOnBundleSymbolicName bsn) {
+		operations.addOnInfo(bsn);
 	}
 	
 	@CliCommand(value="addon list", help="List all known Spring Roo Add-ons (up to the maximum number displayed on a single page)")
 	public void list(@CliOption(key="refresh", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Refresh the add-on index from the Internet") boolean refresh,
-			@CliOption(key="linesPerResult", mandatory=false, unspecifiedDefaultValue="1", specifiedDefaultValue="1", help="The maximum number of lines displayed per add-on") int linesPerResult,
+			@CliOption(key="linesPerResult", mandatory=false, unspecifiedDefaultValue="2", specifiedDefaultValue="2", help="The maximum number of lines displayed per add-on") int linesPerResult,
 			@CliOption(key="maxResults", mandatory=false, unspecifiedDefaultValue="99", specifiedDefaultValue="99", help="The maximum number of add-ons to list") int maxResults,
 			@CliOption(key="trustedOnly", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Only display trusted add-ons in search results") boolean trustedOnly,
 			@CliOption(key="compatibleOnly", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Only display compatible add-ons in search results") boolean compatibleOnly) {
@@ -58,23 +56,22 @@ public class AddOnCommands implements CommandMarker {
 	@CliCommand(value="addon search", help="Search all known Spring Roo Add-ons")
 	public void search(@CliOption(key="requiresDescription", mandatory=false, help="A comma separated list of search terms") String searchTerms,
 			@CliOption(key="refresh", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Refresh the add-on index from the Internet") boolean refresh,
-			@CliOption(key="linesPerResult", mandatory=false, unspecifiedDefaultValue="1", specifiedDefaultValue="1", help="The maximum number of lines displayed per add-on") int linesPerResult,
-			@CliOption(key="maxResults", mandatory=false, unspecifiedDefaultValue="25", specifiedDefaultValue="25", help="The maximum number of add-ons to list") int maxResults,
+			@CliOption(key="linesPerResult", mandatory=false, unspecifiedDefaultValue="2", specifiedDefaultValue="2", help="The maximum number of lines displayed per add-on") int linesPerResult,
+			@CliOption(key="maxResults", mandatory=false, unspecifiedDefaultValue="20", specifiedDefaultValue="20", help="The maximum number of add-ons to list") int maxResults,
 			@CliOption(key="trustedOnly", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Only display trusted add-ons in search results") boolean trustedOnly,
 			@CliOption(key="compatibleOnly", mandatory=false, unspecifiedDefaultValue="false", specifiedDefaultValue="true", help="Only display compatible add-ons in search results") boolean compatibleOnly,
 			@CliOption(key="requiresCommand", mandatory=false, help="Only display add-ons in search results that offer this command") String requiresCommand) {
 		operations.searchAddOns(true, searchTerms, refresh, linesPerResult, maxResults, trustedOnly, compatibleOnly, requiresCommand);
 	}
 	
-	@CliCommand(value="addon install", help="Install Spring Roo Add-on")
-	public void install(@CliOption(key={"", "bundleId"}, mandatory=false, help="The bundle ID as presented via the addon list or addon search command") String bundleId,
-			@CliOption(key="bundleSymbolicName", mandatory=false, help="The bundle symbolic name for the add-on of interest") AddOnBundleSymbolicName bsn) {
-		Assert.isTrue(bsn != null || (bundleId != null && bundleId.length() > 0), "Either a --bundleId (from a search) or --bundleSymbolicName is required");
-		if (bsn != null) {
-			operations.installAddOn(bsn);
-		} else {
-			operations.installAddOn(bundleId);
-		}
+	@CliCommand(value="addon install id", help="Install Spring Roo Add-on")
+	public void installId(@CliOption(key={"", "searchResultId"}, mandatory=true, help="The bundle ID as presented via the addon list or addon search command") String bundleId) {
+		operations.installAddOn(bundleId);
+	}
+	
+	@CliCommand(value="addon install bundle", help="Install Spring Roo Add-on")
+	public void installBsn(@CliOption(key="bundleSymbolicName", mandatory=true, help="The bundle symbolic name for the add-on of interest") AddOnBundleSymbolicName bsn) {
+		operations.installAddOn(bsn);
 	}
 	
 	@CliCommand(value="addon remove", help="Remove Spring Roo Add-on")
