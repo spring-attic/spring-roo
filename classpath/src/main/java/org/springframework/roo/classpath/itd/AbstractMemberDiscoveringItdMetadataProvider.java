e1f28e32df6391d6f9d1b7a72b655b9fe7b8b82a
Ben Alex <balex@vmware.com>
2011-01-07 08:32:35 +0100

    ROO-1883: Introduce AbstractMemberDiscoveringItdMetadataProvider to allow subclasses to easily become aware of newly-discovered ITD metadata - fix MID parsing bug

--- classpath/src/main/java/org/springframework/roo/classpath/itd/AbstractMemberDiscoveringItdMetadataProvider.java
+++ classpath/src/main/java/org/springframework/roo/classpath/itd/AbstractMemberDiscoveringItdMetadataProvider.java
@@ -73,7 +73,7 @@ public abstract class AbstractMemberDiscoveringItdMetadataProvider extends Abstr
 		String localMid = getLocalMidToRequest(itdTypeDetails);
 		if (localMid != null) {
 			Assert.isTrue(MetadataIdentificationUtils.isIdentifyingInstance(localMid), "Metadata identification string '" + localMid + "' should identify a specific instance to request");
-			Assert.isTrue(MetadataIdentificationUtils.getMetadataClass(localMid).equals(getProvidesType()), "Metadata identication string '" + localMid + "' is incompatible with this metadata provider's class");
+			Assert.isTrue(MetadataIdentificationUtils.getMetadataClass(localMid).equals(MetadataIdentificationUtils.getMetadataClass(getProvidesType())), "Metadata identication string '" + MetadataIdentificationUtils.getMetadataClass(localMid) + "' is incompatible with this metadata provider's class '" + MetadataIdentificationUtils.getMetadataClass(getProvidesType()) + "'");
 			metadataService.get(localMid, true);
 		}
 	}
