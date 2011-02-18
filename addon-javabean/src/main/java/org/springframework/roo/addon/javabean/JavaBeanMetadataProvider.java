4191c523f54bb6782550402f40d981a5b07f1744
Alan Stewart <stewarta@vmware.com>
2011-02-18 20:23:50 +1100

    ROO-2013: Remove BeanInfoMetadata from addon-javabean

--- addon-javabean/src/main/java/org/springframework/roo/addon/javabean/JavaBeanMetadataProvider.java
+++ addon-javabean/src/main/java/org/springframework/roo/addon/javabean/JavaBeanMetadataProvider.java
@@ -7,7 +7,6 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.component.ComponentContext;
-import org.springframework.roo.addon.beaninfo.BeanInfoMetadataProvider;
 import org.springframework.roo.classpath.PhysicalTypeDetails;
 import org.springframework.roo.classpath.PhysicalTypeIdentifier;
 import org.springframework.roo.classpath.PhysicalTypeMetadata;
@@ -20,6 +19,7 @@ import org.springframework.roo.classpath.itd.ItdTypeDetailsProvidingMetadataItem
 import org.springframework.roo.model.JavaType;
 import org.springframework.roo.project.Path;
 import org.springframework.roo.project.ProjectMetadata;
+import org.springframework.roo.project.ProjectOperations;
 
 /**
  * Provides {@link JavaBeanMetadata}.
@@ -30,24 +30,22 @@ import org.springframework.roo.project.ProjectMetadata;
 @Component(immediate = true) 
 @Service 
 public final class JavaBeanMetadataProvider extends AbstractItdMetadataProvider {
-	@Reference private BeanInfoMetadataProvider beanInfoMetadataProvider;
+	@Reference private ProjectOperations projectOperations;
 	@Reference private TypeLocationService typeLocationService;
 	
 	protected void activate(ComponentContext context) {
 		metadataDependencyRegistry.registerDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
-		beanInfoMetadataProvider.addMetadataTrigger(new JavaType(RooJavaBean.class.getName()));
 		addMetadataTrigger(new JavaType(RooJavaBean.class.getName()));
 	}
 
 	protected void deactivate(ComponentContext context) {
 		metadataDependencyRegistry.deregisterDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(), getProvidesType());
-		beanInfoMetadataProvider.removeMetadataTrigger(new JavaType(RooJavaBean.class.getName()));
 		removeMetadataTrigger(new JavaType(RooJavaBean.class.getName()));
 	}
 
 	protected ItdTypeDetailsProvidingMetadataItem getMetadata(String metadataIdentificationString, JavaType aspectName, PhysicalTypeMetadata governorPhysicalTypeMetadata, String itdFilename) {
 		// Work out the MIDs of the other metadata we depend on
-		ProjectMetadata projectMetadata = (ProjectMetadata) metadataService.get(ProjectMetadata.getProjectIdentifier());
+		ProjectMetadata projectMetadata = projectOperations.getProjectMetadata();
 		if (projectMetadata == null || !projectMetadata.isValid()) {
 			return null;
 		}
