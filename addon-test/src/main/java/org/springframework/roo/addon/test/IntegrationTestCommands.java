4a185b0566865dc9031735bf740c4655ef87d73c
Alan Stewart <stewarta@vmware.com>
2011-03-23 17:20:38 +1100

    ROO-2257: Move 'test integration' and 'dod' commands into addon-test and addon-dod respectively - added missing classes to this commit

--- addon-test/src/main/java/org/springframework/roo/addon/test/IntegrationTestCommands.java
+++ addon-test/src/main/java/org/springframework/roo/addon/test/IntegrationTestCommands.java
@@ -10,6 +10,7 @@ import org.springframework.roo.shell.CliAvailabilityIndicator;
 import org.springframework.roo.shell.CliCommand;
 import org.springframework.roo.shell.CliOption;
 import org.springframework.roo.shell.CommandMarker;
+import org.springframework.roo.support.util.Assert;
 
 /**
  * Shell commands for {@link IntegrationTestOperationsImpl}.
@@ -23,11 +24,30 @@ public class IntegrationTestCommands implements CommandMarker {
 	@Reference private IntegrationTestOperations integrationTestOperations;
 	@Reference private ProjectOperations projectOperations;
 
+	@CliAvailabilityIndicator( { "test integration" })
+	public boolean isPersistentClassAvailable() {
+		return integrationTestOperations.isPersistentClassAvailable();
+	}
+	
 	@CliAvailabilityIndicator({ "test mock", "test stub" })
 	public boolean isAvailable() {
 		return projectOperations.isProjectAvailable();
 	}
-	
+
+	@CliCommand(value = "test integration", help = "Creates a new integration test for the specified entity")
+	public void newIntegrationTest(
+		@CliOption(key = "entity", mandatory = false, unspecifiedDefaultValue = "*", optionContext = "update,project", help = "The name of the entity to create an integration test for") JavaType entity, 
+		@CliOption(key = "permitReservedWords", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Indicates whether reserved words are ignored by Roo") boolean permitReservedWords) {
+
+		if (!permitReservedWords) {
+			ReservedWords.verifyReservedWordsNotPresent(entity);
+		}
+
+		Assert.isTrue(isEntityReasonablyNamed(entity), "Cannot create an integration test for an entity named 'Test' or 'TestCase' under any circumstances");
+		
+		integrationTestOperations.newIntegrationTest(entity);
+	}
+
 	@CliCommand(value = "test mock", help = "Creates a mock test for the specified entity")
 	public void newMockTest(
 		@CliOption(key = "entity", mandatory = false, unspecifiedDefaultValue = "*", optionContext = "update,project", help = "The name of the entity this mock test is targeting") JavaType entity, 
@@ -50,5 +70,9 @@ public class IntegrationTestCommands implements CommandMarker {
 		}
 
 		integrationTestOperations.newTestStub(entity);
+	}
+	
+	private boolean isEntityReasonablyNamed(JavaType entity) {
+		return !entity.getSimpleTypeName().startsWith("Test") && !entity.getSimpleTypeName().endsWith("TestCase") && !entity.getSimpleTypeName().endsWith("Test");
 	}
 }
--- addon-test/src/main/java/org/springframework/roo/addon/test/IntegrationTestOperations.java
+++ addon-test/src/main/java/org/springframework/roo/addon/test/IntegrationTestOperations.java
@@ -10,6 +10,21 @@ import org.springframework.roo.model.JavaType;
 public interface IntegrationTestOperations {
 
 	/**
+	 * Checks for the existence the META-INF/persistence.xml
+	 * 
+	 * @return true if the META-INF/persistence.xml exists, otherwise false
+	 */
+	boolean isPersistentClassAvailable();
+
+	/**
+	 * Creates an integration test for the entity. Automatically produces a data-on-demand (DoD) class if one does not exist.
+	 * Silently returns if the integration test file already exists.
+	 * 
+	 * @param entity the entity to produce an integration test for (required)
+	 */
+	void newIntegrationTest(JavaType entity);
+
+	/**
 	 * Creates a mock test for the entity. Silently returns if the mock test file already exists.
 	 * 
 	 * @param entity to produce a mock test for (required)
