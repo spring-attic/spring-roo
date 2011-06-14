b37cff10130e645cb27834cd52a6326781d88278
Stefan Schmidt <schmidts@vmware.com>
2011-06-14 10:36:50 +1000

    ROO-301: Choose between data access patterns - defining Annotation targets and source retention

--- addon-layers-repository/src/main/java/org/springframework/roo/addon/layers/repository/RooRepositoryJpa.java
+++ addon-layers-repository/src/main/java/org/springframework/roo/addon/layers/repository/RooRepositoryJpa.java
@@ -1,10 +1,17 @@
 package org.springframework.roo.addon.layers.repository;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 
  * @author Stefan Schmidt
  * @since 1.2
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface RooRepositoryJpa {
 	
 	public static final String REMOVE_METHOD = "remove";
--- addon-layers-service/src/main/java/org/springframework/roo/addon/layers/service/RooService.java
+++ addon-layers-service/src/main/java/org/springframework/roo/addon/layers/service/RooService.java
@@ -1,10 +1,17 @@
 package org.springframework.roo.addon.layers.service;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 
  * @author Stefan Schmidt
  * @since 1.2
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface RooService {
 	public static final String DOMAIN_TYPES = "domainTypes";
 	
