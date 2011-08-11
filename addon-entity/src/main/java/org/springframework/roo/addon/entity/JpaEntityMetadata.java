ed07b6a0e4911cb5cebf690c957a942b2caa60e7
Alan Stewart <stewarta@vmware.com>
2011-08-11 18:02:51 +1000

    Revert "ROO-2617: New @RooJpaEntity annotation to manage core JPA entity concerns - fixed bug where Identitier was not being checked first for for id field name"
    This reverts commit 9e8afe6f3fdffa236059ea6c21d52ab4d9ee15ba.

--- addon-entity/src/main/java/org/springframework/roo/addon/entity/JpaEntityMetadata.java
+++ addon-entity/src/main/java/org/springframework/roo/addon/entity/JpaEntityMetadata.java
@@ -325,10 +325,10 @@ public class JpaEntityMetadata extends AbstractItdTypeDetailsProvidingMetadataIt
 	}
 	
 	private String getIdentifierFieldName() {
-		if (identifier != null && identifier.getFieldName() != null) {
-			return identifier.getFieldName().getSymbolName();
-		} else if (StringUtils.hasText(annotationValues.getIdentifierField())) {
+		if (StringUtils.hasText(annotationValues.getIdentifierField())) {
 			return annotationValues.getIdentifierField();
+		} else if (identifier != null && identifier.getFieldName() != null) {
+			return identifier.getFieldName().getSymbolName();
 		}
 		// Use the default
 		return RooJpaEntity.ID_FIELD_DEFAULT;
