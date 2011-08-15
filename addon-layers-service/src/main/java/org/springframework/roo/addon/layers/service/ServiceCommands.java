72b4be5fb115da7942a02d82172c2b30c041aa44
Andrew Swan <aswan@vmware.com>
2011-08-16 09:54:48 +1000

    ROO-301: Choose between data access patterns - shortened addon names for nicer display in module list

--- addon-layers-service/src/main/java/org/springframework/roo/addon/layers/service/ServiceCommands.java
+++ addon-layers-service/src/main/java/org/springframework/roo/addon/layers/service/ServiceCommands.java
@@ -10,6 +10,7 @@ import org.springframework.roo.shell.CliOption;
 import org.springframework.roo.shell.CommandMarker;
 
 /**
+ * Shell commands that create domain services.
  * 
  * @author Stefan Schmidt
  * @since 1.2.0
@@ -17,7 +18,8 @@ import org.springframework.roo.shell.CommandMarker;
 @Component
 @Service
 public class ServiceCommands implements CommandMarker {
-	
+
+	// Fields
 	@Reference private ServiceOperations serviceOperations;
 	
 	@CliAvailabilityIndicator("service")
@@ -28,12 +30,11 @@ public class ServiceCommands implements CommandMarker {
 	@CliCommand(value = "service", help = "Adds @RooService annotation to target type") 
 	public void service(@CliOption(key = "interface", mandatory = true, help = "The java interface to apply this annotation to") JavaType interfaceType,
 			@CliOption(key = "class", mandatory = false, help = "Implementation class for the specified interface") JavaType classType,
-			@CliOption(key = "entity", unspecifiedDefaultValue = "*", optionContext = "update,project", mandatory = false, help = "The domain entity this service should expose") JavaType domainType) {
-		
+			@CliOption(key = "entity", unspecifiedDefaultValue = "*", optionContext = "update,project", mandatory = false, help = "The domain entity this service should expose") JavaType domainType)
+	{
 		if (classType == null) {
 			classType = new JavaType(interfaceType.getFullyQualifiedTypeName() + "Impl");
 		}
 		serviceOperations.setupService(interfaceType, classType, domainType);
 	}
-
 }
