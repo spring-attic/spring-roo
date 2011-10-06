1fde2b8a92b3cab706ff182673d23666e31e7139
Andrew Swan <aswan@vmware.com>
2011-10-06 13:09:42 +1100

    ROO-2812: Make output of "metadata status" command more readable

--- metadata/src/main/java/org/springframework/roo/metadata/internal/StandardMetadataTimingStatistic.java
+++ metadata/src/main/java/org/springframework/roo/metadata/internal/StandardMetadataTimingStatistic.java
@@ -11,15 +11,34 @@ import org.springframework.roo.support.util.Assert;
  */
 public class StandardMetadataTimingStatistic implements MetadataTimingStatistic {
 
+	// Constants
+	static final long NANOSECONDS_IN_MILLISECOND = 1000000L;
+	
+	// Chosen to match the maximum
+	private static final int MAXIMUM_EXPECTED_INVOCATIONS = 5;
+	
+	private static final String INVOCATION_COUNT_FORMAT = "%" + MAXIMUM_EXPECTED_INVOCATIONS + "d";
+	private static final String TIME_FORMAT = "%" + (String.valueOf(NANOSECONDS_IN_MILLISECOND).length() - 1) + "d";
+	
+	// Fields
 	private final String name;
-	private final long time;
+	private final long nanoseconds;
 	private final long invocations;
 
-	public StandardMetadataTimingStatistic(final String name, final long time, final long invocations) {
+	/**
+	 * Constructor
+	 *
+	 * @param name (required)
+	 * @param nanoseconds the elasped time in nanoseconds (zero or more)
+	 * @param invocations (zero or more)
+	 */
+	public StandardMetadataTimingStatistic(final String name, final long nanoseconds, final long invocations) {
 		Assert.hasText(name, "Name required");
-		this.name = name;
-		this.time = time;
+		Assert.isTrue(invocations >= 0, "Invocations must be zero or more");
+		Assert.isTrue(nanoseconds >= 0, "Nanoseconds must be zero or more");
 		this.invocations = invocations;
+		this.name = name;
+		this.nanoseconds = nanoseconds;
 	}
 
 	public String getName() {
@@ -27,7 +46,7 @@ public class StandardMetadataTimingStatistic implements MetadataTimingStatistic 
 	}
 
 	public long getTime() {
-		return time;
+		return nanoseconds;
 	}
 
 	public long getInvocations() {
@@ -35,7 +54,7 @@ public class StandardMetadataTimingStatistic implements MetadataTimingStatistic 
 	}
 
 	public int compareTo(final MetadataTimingStatistic o) {
-		int result = Long.valueOf(time).compareTo(o.getTime());
+		int result = Long.valueOf(nanoseconds).compareTo(o.getTime());
 		if (result == 0) {
 			result = Long.valueOf(invocations).compareTo(o.getInvocations());
 		}
@@ -52,20 +71,20 @@ public class StandardMetadataTimingStatistic implements MetadataTimingStatistic 
 
 	@Override
 	public int hashCode() {
-		return Long.valueOf(time).hashCode() * Long.valueOf(invocations).hashCode() * name.hashCode();
+		return Long.valueOf(nanoseconds).hashCode() * Long.valueOf(invocations).hashCode() * name.hashCode();
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		if (time < 1000000) {
-			// Nanosecond precision
-			sb.append(String.format("%06d", time)).append(" ns; ");
+		if (nanoseconds < NANOSECONDS_IN_MILLISECOND) {
+			// Display as nanoseconds
+			sb.append(String.format(TIME_FORMAT, nanoseconds)).append(" ns; ");
 		} else {
-			// Millisecond precision
-			sb.append(String.format("%06d", time/1000000)).append(" ms; ");
+			// Display as milliseconds
+			sb.append(String.format(TIME_FORMAT, nanoseconds/1000000)).append(" ms; ");
 		}
-		sb.append(String.format("%06d", invocations)).append(" call(s): ");
+		sb.append(String.format(INVOCATION_COUNT_FORMAT, invocations)).append(" call(s): ");
 		sb.append(name);
 		return sb.toString();
 	}
---
+++ metadata/src/test/java/org/springframework/roo/metadata/internal/StandardMetadataTimingStatisticTest.java
@@ -0,0 +1,56 @@
+package org.springframework.roo.metadata.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.roo.metadata.internal.StandardMetadataTimingStatistic.NANOSECONDS_IN_MILLISECOND;
+
+import org.junit.Test;
+
+/**
+ * Unit test of {@link StandardMetadataTimingStatistic}
+ *
+ * @author Andrew Swan
+ * @since 1.2.0
+ */
+public class StandardMetadataTimingStatisticTest {
+
+	// Constants
+	private static final long INVOCATIONS = 5;
+	
+	private static final String NAME = "MyProcess";
+
+	@Test
+	public void testToStringForLessThanOneMillisecond() {
+		assertToString(NANOSECONDS_IN_MILLISECOND - 1, "999999 ns;     5 call(s): MyProcess");
+	}
+	
+	@Test
+	public void testToStringForOneMillisecond() {
+		assertToString(NANOSECONDS_IN_MILLISECOND,     "     1 ms;     5 call(s): MyProcess");
+	}
+	
+	@Test
+	public void testToStringForTwoMilliseconds() {
+		assertToString(NANOSECONDS_IN_MILLISECOND * 2, "     2 ms;     5 call(s): MyProcess");
+	}
+	
+	/**
+	 * Asserts that calling {@link StandardMetadataTimingStatistic#toString()}
+	 * on an instance with the given duration results in the expected output
+	 * 
+	 * @param nanoseconds
+	 * @param expectedToString
+	 */
+	private void assertToString(final long nanoseconds, final String expectedToString) {
+		assertEquals(expectedToString, getTestInstance(nanoseconds).toString());
+	}
+	
+	/**
+	 * Creates an instance with fixed test values and the given duration
+	 * 
+	 * @param nanoseconds
+	 * @return a non-<code>null</code> instance
+	 */
+	private StandardMetadataTimingStatistic getTestInstance(final long nanoseconds) {
+		return new StandardMetadataTimingStatistic(NAME, nanoseconds, INVOCATIONS);
+	}
+}
