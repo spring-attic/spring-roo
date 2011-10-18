b212eaaa572e0d9c24a1e8d6eb2d2be845aac78c
Andrew Swan <aswan@vmware.com>
2011-10-18 13:30:11 +1100

    ROO-2815: Create script to demonstrate embedding options - removed persistence setup, now running script from roo-deploy

--- deployment-support/roo-deploy.sh
+++ deployment-support/roo-deploy.sh
@@ -535,6 +535,9 @@ if [[ "$COMMAND" = "assembly" ]]; then
         load_roo_build_and_test script bikeshop.roo
         tomcat_stop_start_get_stop http://localhost:8888/bikeshop
 
+        load_roo_build_and_test script embedding.roo
+        tomcat_stop_start_get_stop http://localhost:8888/embedding
+
         log "Removing Roo distribution from test area"
         rm -rf /tmp/$RELEASE_IDENTIFIER
         log "Completed tests successfully"
