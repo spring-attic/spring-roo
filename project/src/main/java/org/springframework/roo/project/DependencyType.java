bc675b25bb37960bff4b9b2c85d470fffdb88712
Andrew Swan <aswan@vmware.com>
2011-11-15 10:35:44 +0100

    ROO-120: Support multi module Maven projects - added WAR as a DependencyType

--- project/src/main/java/org/springframework/roo/project/DependencyType.java
+++ project/src/main/java/org/springframework/roo/project/DependencyType.java
@@ -1,6 +1,7 @@
 package org.springframework.roo.project;
 
 import org.springframework.roo.support.util.DomUtils;
+import org.springframework.roo.support.util.StringUtils;
 import org.springframework.roo.support.util.XmlUtils;
 import org.w3c.dom.Element;
 
@@ -11,34 +12,50 @@ import org.w3c.dom.Element;
  * @since 1.0
  */
 public enum DependencyType {
+	
 	JAR,
+	WAR,
 	ZIP,
 	OTHER;
 
 	/**
 	 * Returns the type of the dependency represented by the given XML element
 	 *
-	 * @param dependency the element from which to parse the type (required)
+	 * @param dependencyElement the element from which to parse the type (required)
 	 * @return a non-<code>null</code> type
 	 * @since 1.2.0
 	 */
-	public static DependencyType getType(final Element dependency) {
+	public static DependencyType getType(final Element dependencyElement) {
 		// Find the type code, if any
-		final String type;
-		if (dependency.hasAttribute("type")) {
-			type = dependency.getAttribute("type");
-		} else {
-			// Read it from the "type" child element, if any
-			type = DomUtils.getTextContent(XmlUtils.findFirstElement("type", dependency), "").trim();
-		}
+		final String type = getTypeCode(dependencyElement);
 
 		// Resolve this to a DependencyType
-		if ("".equals(type) || "jar".equalsIgnoreCase(type)) {
+		return valueOfTypeCode(type);
+	}
+
+	/**
+	 * Returns the {@link DependencyType} with the given code.
+	 * 
+	 * @param typeCode the type code to decode (can be anything, case doesn't matter)
+	 * @return {@link #OTHER} if the given code is non-blank and unrecognised
+	 * @since 1.2.0
+	 */
+	public static DependencyType valueOfTypeCode(final String typeCode) {
+		if (StringUtils.isBlank(typeCode)) {
 			return JAR;
 		}
-		if ("zip".equalsIgnoreCase(type)) {
-			return ZIP;
+		try {
+			return valueOf(typeCode);
+		} catch (final IllegalArgumentException invalidCode) {
+			return OTHER;
 		}
-		return OTHER;
+	}
+
+	private static String getTypeCode(final Element dependencyElement) {
+		if (dependencyElement.hasAttribute("type")) {
+			return dependencyElement.getAttribute("type");
+		}
+		// Read it from the "type" child element, if any
+		return DomUtils.getTextContent(XmlUtils.findFirstElement("type", dependencyElement), "").trim();
 	}
 }
---
+++ project/src/test/java/org/springframework/roo/project/DependencyTypeTest.java
@@ -0,0 +1,36 @@
+package org.springframework.roo.project;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test of the {@link DependencyType} enum.
+ *
+ * @author Andrew Swan
+ * @since 1.2.0
+ */
+public class DependencyTypeTest {
+
+	@Test
+	public void testValueOfNullCode() {
+		assertEquals(DependencyType.JAR, DependencyType.valueOfTypeCode(null));
+	}
+
+	@Test
+	public void testValueOfEmptyCode() {
+		assertEquals(DependencyType.JAR, DependencyType.valueOfTypeCode(""));
+	}
+
+	@Test
+	public void testValueOfUnknownCode() {
+		assertEquals(DependencyType.OTHER, DependencyType.valueOfTypeCode("guff"));
+	}
+	
+	@Test
+	public void testValueOfKnownCode() {
+		for (final DependencyType dependencyType : DependencyType.values()) {
+			assertEquals(dependencyType, DependencyType.valueOfTypeCode(dependencyType.name()));
+		}
+	}
+}
