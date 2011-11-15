d2182025e61fd5715d8a769a5b390048f7f3d676
Andrew Swan <aswan@vmware.com>
2011-11-15 14:51:30 +0100

    ROO-120: Support multi module Maven projects - added core "war" PackagingProvider

---
+++ project/src/main/java/org/springframework/roo/project/packaging/WarPackaging.java
@@ -0,0 +1,33 @@
+package org.springframework.roo.project.packaging;
+
+import static org.springframework.roo.project.Path.SPRING_CONFIG_ROOT;
+import static org.springframework.roo.project.Path.SRC_MAIN_JAVA;
+import static org.springframework.roo.project.Path.SRC_MAIN_WEBAPP;
+import static org.springframework.roo.project.Path.SRC_TEST_JAVA;
+import static org.springframework.roo.project.Path.SRC_TEST_RESOURCES;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.project.Path;
+
+/**
+ * The core {@link PackagingProvider} for web modules.
+ *
+ * @author Andrew Swan
+ * @since 1.2.0
+ */
+@Component
+@Service
+public class WarPackaging extends AbstractCorePackagingProvider {
+
+	public WarPackaging() {
+		super("war", "war-pom-template.xml");
+	}
+
+	public Collection<Path> getPaths() {
+		return Arrays.asList(SRC_MAIN_JAVA, SRC_TEST_JAVA, SRC_TEST_RESOURCES, SPRING_CONFIG_ROOT, SRC_MAIN_WEBAPP);
+	}
+}
