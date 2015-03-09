508a8cd3e8a4d7b0270d86dcb9b5386d8ef48ace
Juan Carlos García <jugade92@gmail.com>
2015-03-09 13:13:20 +0100

    Adding Addon-Search OSGi support using installed OBR

--- runtime/obr-addon-search/src/main/java/org/springframework/roo/obr/addon/search/ObrAddOnCommands.java
+++ runtime/obr-addon-search/src/main/java/org/springframework/roo/obr/addon/search/ObrAddOnCommands.java
@@ -48,22 +48,6 @@ public class ObrAddOnCommands implements CommandMarker {
     	operations.installAddOn(bundleId);
     }
 
-    @CliCommand(value = "addon list", help = "List all known Spring Roo Add-ons (up to the maximum number displayed on a single page)")
-    public void list(
-            @CliOption(key = "refresh", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Refresh the add-on index from the Internet") final boolean refresh,
-            @CliOption(key = "linesPerResult", mandatory = false, unspecifiedDefaultValue = "2", specifiedDefaultValue = "2", help = "The maximum number of lines displayed per add-on") final int linesPerResult,
-            @CliOption(key = "maxResults", mandatory = false, unspecifiedDefaultValue = "99", specifiedDefaultValue = "99", help = "The maximum number of add-ons to list") final int maxResults,
-            @CliOption(key = "trustedOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display trusted add-ons in search results") final boolean trustedOnly,
-            @CliOption(key = "communityOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display community provided add-ons in search results") final boolean communityOnly,
-            @CliOption(key = "compatibleOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display compatible add-ons in search results") final boolean compatibleOnly) {
-
-        // A list is really just a search without criteria. We keep some
-        // criteria to allow reasonable filtering and display logic to take
-        // place.
-    	operations.searchAddOns(true, null, refresh, linesPerResult,
-                maxResults, trustedOnly, compatibleOnly, communityOnly, null);
-    }
-
     @CliCommand(value = "addon remove", help = "Remove Spring Roo Add-on")
     public void remove(
             @CliOption(key = "bundleSymbolicName", mandatory = true, help = "The bundle symbolic name for the add-on of interest") final BundleSymbolicName bsn) {
@@ -73,18 +57,10 @@ public class ObrAddOnCommands implements CommandMarker {
 
     @CliCommand(value = "addon search", help = "Search all known Spring Roo Add-ons")
     public void search(
-            @CliOption(key = { "", "requiresDescription" }, mandatory = false, specifiedDefaultValue = "*", unspecifiedDefaultValue = "*", help = "A comma separated list of search terms") final String searchTerms,
-            @CliOption(key = "refresh", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Refresh the add-on index from the Internet") final boolean refresh,
-            @CliOption(key = "linesPerResult", mandatory = false, unspecifiedDefaultValue = "2", specifiedDefaultValue = "2", help = "The maximum number of lines displayed per add-on") final int linesPerResult,
-            @CliOption(key = "maxResults", mandatory = false, unspecifiedDefaultValue = "20", specifiedDefaultValue = "20", help = "The maximum number of add-ons to list") final int maxResults,
-            @CliOption(key = "trustedOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display trusted add-ons in search results") final boolean trustedOnly,
-            @CliOption(key = "compatibleOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display compatible add-ons in search results") final boolean compatibleOnly,
-            @CliOption(key = "communityOnly", mandatory = false, unspecifiedDefaultValue = "false", specifiedDefaultValue = "true", help = "Only display community provided add-ons in search results") final boolean communityOnly,
-            @CliOption(key = "requiresCommand", mandatory = false, help = "Only display add-ons in search results that offer this command") final String requiresCommand) {
+            @CliOption(key = "requiresCommand", mandatory = true, help = "Only display add-ons in search results that offer this command") final String requiresCommand) {
 
-    	operations.searchAddOns(true, searchTerms, refresh,
-                linesPerResult, maxResults, trustedOnly, compatibleOnly,
-                communityOnly, requiresCommand);
+    	operations.searchAddOns(false, null, false, 1, 99, false,
+                false, false, requiresCommand);
     }
 
 }
