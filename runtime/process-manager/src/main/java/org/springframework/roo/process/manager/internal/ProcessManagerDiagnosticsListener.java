c351406b2466a79584798e67f39670f9e427129c
Juan Carlos García <jugade92@gmail.com>
2015-04-22 10:41:28 +0200

    ROO-3612: process manager command must appear only with development mode

--- runtime/process-manager/src/main/java/org/springframework/roo/process/manager/internal/ProcessManagerDiagnosticsListener.java
+++ runtime/process-manager/src/main/java/org/springframework/roo/process/manager/internal/ProcessManagerDiagnosticsListener.java
@@ -1,25 +1,33 @@
 package org.springframework.roo.process.manager.internal;
 
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
 import org.springframework.roo.process.manager.ProcessManager;
 import org.springframework.roo.process.manager.event.ProcessManagerStatus;
 import org.springframework.roo.process.manager.event.ProcessManagerStatusListener;
 import org.springframework.roo.process.manager.event.ProcessManagerStatusProvider;
+import org.springframework.roo.shell.CliAvailabilityIndicator;
 import org.springframework.roo.shell.CliCommand;
 import org.springframework.roo.shell.CliOption;
 import org.springframework.roo.shell.CommandMarker;
+import org.springframework.roo.shell.Shell;
 import org.springframework.roo.shell.osgi.AbstractFlashingObject;
+import org.springframework.roo.support.logging.HandlerUtils;
 
 /**
  * Allows monitoring of {@link ProcessManager} for development mode users.
  * 
  * @author Ben Alex
  * @author Stefan Schmidt
+ * @author Juan Carlos García
  * @since 1.1
  */
 @Service
@@ -27,16 +35,31 @@ import org.springframework.roo.shell.osgi.AbstractFlashingObject;
 public class ProcessManagerDiagnosticsListener extends AbstractFlashingObject
         implements ProcessManagerStatusListener, CommandMarker {
 
-    private boolean isDebug = false;
+    private static final String PROCESS_MANAGER_DEBUG_COMMAND = "process manager debug";
+    
+    
+	private boolean isDebug = false;
     @Reference private ProcessManagerStatusProvider processManagerStatusProvider;
-
+    
+    protected final static Logger LOGGER = HandlerUtils.getLogger(
+    		ProcessManagerDiagnosticsListener.class);
+	
+    // ------------ OSGi component attributes ----------------
+   	private BundleContext context;
+    
     protected void activate(final ComponentContext context) {
+    	this.context = context.getBundleContext();
         processManagerStatusProvider.addProcessManagerStatusListener(this);
         isDebug = System.getProperty("roo-args") != null && isDevelopmentMode();
     }
 
     protected void deactivate(final ComponentContext context) {
         processManagerStatusProvider.removeProcessManagerStatusListener(this);
+    }
+    
+    @CliAvailabilityIndicator(PROCESS_MANAGER_DEBUG_COMMAND)
+    public boolean isProcessManagerDebugAvailable() {
+    	return getShell().isDevelopmentMode() && getProcessManager().isDevelopmentMode();
     }
 
     public void onProcessManagerStatusChange(
@@ -46,10 +69,44 @@ public class ProcessManagerDiagnosticsListener extends AbstractFlashingObject
             flash(Level.FINE, newStatus.name(), MY_SLOT);
         }
     }
-
-    @CliCommand(value = "process manager debug", help = "Indicates if process manager debugging is desired")
+    
+    @CliCommand(value = PROCESS_MANAGER_DEBUG_COMMAND, help = "Indicates if process manager debugging is desired")
     public void processManagerDebug(
             @CliOption(key = { "", "enabled" }, mandatory = false, specifiedDefaultValue = "true", unspecifiedDefaultValue = "true", help = "Activates debug mode") final boolean debug) {
         isDebug = debug;
+    }
+    
+    public ProcessManager getProcessManager(){
+    	// Get all components implement ProcessManager interface
+		try {
+			ServiceReference<?>[] references = this.context.getAllServiceReferences(ProcessManager.class.getName(), null);
+			
+			for(ServiceReference<?> ref : references){
+				return (ProcessManager) this.context.getService(ref);
+			}
+			
+			return null;
+			
+		} catch (InvalidSyntaxException e) {
+			LOGGER.warning("Cannot load ProcessManager on ProcessManagerDiagnosticListener.");
+			return null;
+		}
+    }
+    
+    public Shell getShell(){
+    	// Get all Shell implement Shell interface
+		try {
+			ServiceReference<?>[] references = this.context.getAllServiceReferences(Shell.class.getName(), null);
+			
+			for(ServiceReference<?> ref : references){
+				return (Shell) this.context.getService(ref);
+			}
+			
+			return null;
+			
+		} catch (InvalidSyntaxException e) {
+			LOGGER.warning("Cannot load Shell on ProcessManagerDiagnosticListener.");
+			return null;
+		}
     }
 }
