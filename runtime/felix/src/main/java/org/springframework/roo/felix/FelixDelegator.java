e9d17339b3ff0953b64ff6d5c4e1389e69708c1f
Juan Carlos García <jugade92@gmail.com>
2015-04-22 12:58:29 +0200

    ROO-3612: Rename osgi framework command with git status

--- runtime/felix/src/main/java/org/springframework/roo/felix/FelixDelegator.java
+++ runtime/felix/src/main/java/org/springframework/roo/felix/FelixDelegator.java
@@ -315,7 +315,7 @@ public class FelixDelegator implements CommandMarker, ShellStatusListener {
         }
     }
 
-    @CliCommand(value = "osgi framework command", help = "Passes a command directly through to the Felix shell infrastructure")
+    @CliCommand(value = "!g", help = "Passes a command directly through to the Felix shell infrastructure")
     public void shell(
             @CliOption(key = "", mandatory = false, specifiedDefaultValue = "help", unspecifiedDefaultValue = "help", help = "The command to pass to Felix (WARNING: no validation or security checks are performed)") final String commandLine)
             throws Exception {
