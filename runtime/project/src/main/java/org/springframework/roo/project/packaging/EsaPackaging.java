d5d0fe3fa664f878a543e6bc70cc905b8770f809
Juan Carlos García del Canto <jugade92@gmail.com>
2015-04-25 14:42:42 +0100

    ROO-3620: Creating new command to generate Roo Addon Suite and its repository

---
+++ runtime/project/src/main/java/org/springframework/roo/project/packaging/EsaPackaging.java
@@ -0,0 +1,37 @@
+package org.springframework.roo.project.packaging;
+
+import java.util.Collection;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.model.JavaPackage;
+import org.springframework.roo.project.Path;
+import org.springframework.roo.project.ProjectOperations;
+
+/**
+ * The Maven "esa" {@link PackagingProvider}
+ * 
+ * @author Juan Carlos García
+ * @since 2.0.0
+ */
+@Component
+@Service
+public class EsaPackaging extends AbstractCorePackagingProvider {
+
+    /**
+     * Constructor
+     */
+    public EsaPackaging() {
+        super("esa", "parent-esa-template.xml");
+    }
+
+    @Override
+    protected void createOtherArtifacts(final JavaPackage topLevelPackage,
+            final String module, final ProjectOperations projectOperations) {
+        // No artifacts are applicable for POM modules
+    }
+
+    public Collection<Path> getPaths() {
+        return null;
+    }
+}
