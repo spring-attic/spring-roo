e468ebd386334fceb7b38be4751548d996e7f58f
jmvivo <jmvivoa1@gmail.com>
2016-10-10 09:13:32 +0200

    ROO-3793 Fix modification lost in rebase

--- runtime/classpath/src/main/java/org/springframework/roo/classpath/operations/Cascade.java
+++ runtime/classpath/src/main/java/org/springframework/roo/classpath/operations/Cascade.java
@@ -3,15 +3,20 @@ package org.springframework.roo.classpath.operations;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
- * This enum type represents javax.persistence.CascadeType 
+ * This enum type represents javax.persistence.CascadeType
  * on Spring Roo Shell
- * 
+ *
  * @author Sergio Clares
  * @since 2.0
  */
 public enum Cascade {
 
   ALL, DETACH, MERGE, PERSIST, REFRESH, REMOVE;
+
+
+  public static final Cascade[] ALL_ARRAY = {ALL};
+
+  public static final Cascade[] MERGE_PERSIST = {MERGE, PERSIST};
 
   @Override
   public String toString() {
