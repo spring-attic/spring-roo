b1c237fe6219cd3e5a11a1d080a572db0ad59455
jcagarcia <jugade92@gmail.com>
2016-10-17 10:01:53 +0200

    ROO-3795: Including new command to add @Preathorize annotation

--- addon-security/addon/src/main/java/org/springframework/roo/addon/security/addon/security/SecurityOperations.java
+++ addon-security/addon/src/main/java/org/springframework/roo/addon/security/addon/security/SecurityOperations.java
@@ -3,6 +3,8 @@ package org.springframework.roo.addon.security.addon.security;
 import java.util.List;
 
 import org.springframework.roo.addon.security.addon.security.providers.SecurityProvider;
+import org.springframework.roo.model.JavaSymbolName;
+import org.springframework.roo.model.JavaType;
 import org.springframework.roo.project.Feature;
 import org.springframework.roo.project.FeatureNames;
 import org.springframework.roo.project.maven.Pom;
@@ -20,19 +22,34 @@ public interface SecurityOperations extends Feature {
   String SECURITY_FEATURE_NAME = FeatureNames.SECURITY;
 
   /**
-   * Defines install operation that will be used by implementations to install the necessary
-   * components of Spring Security.
+   * Defines install operation that will be used by implementations to install
+   * the necessary components of Spring Security.
    * 
-   * @param type SecurityProvider type that will be install
-   * @param module Pom of the application module where Spring Security will be installed.
+   * @param type
+   *            SecurityProvider type that will be install
+   * @param module
+   *            Pom of the application module where Spring Security will be
+   *            installed.
    */
   void installSecurity(SecurityProvider type, Pom module);
 
   /**
-   * Defines getAllSecurityProviders operation that will be used by implementations to
-   * get all security providers registered on the Spring Roo Shell.
+   * Defines getAllSecurityProviders operation that will be used by
+   * implementations to get all security providers registered on the Spring
+   * Roo Shell.
    * 
-   * @return List with the registered security providers on the Spring Roo Shell.
+   * @return List with the registered security providers on the Spring Roo
+   *         Shell.
    */
   List<SecurityProvider> getAllSecurityProviders();
+
+  /**
+   * Defines addPreAuthorizeAnnotation method that will be used by
+   * implementations to include @PreAuthorize annotation in service methods.
+   * 
+   * @param klass
+   * @param methodName
+   * @param value
+   */
+  void addPreAuthorizeAnnotation(JavaType klass, String methodName, String value);
 }
