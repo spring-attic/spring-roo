3a9c9a4d56c952fbefc42e862b663930281b3285
sclaresmt <sclaresmt@gmail.com>
2016-12-14 13:50:20 +0100

    Fix top level package for '@RooLinkFactory'

--- runtime/model/src/main/java/org/springframework/roo/model/RooJavaType.java
+++ runtime/model/src/main/java/org/springframework/roo/model/RooJavaType.java
@@ -132,7 +132,7 @@ public final class RooJavaType {
   public static final JavaType ROO_EXCEPTION_HANDLER = new JavaType(
       "org.springframework.roo.addon.web.mvc.exceptions.annotations.RooExceptionHandler");
   public static final JavaType ROO_LINK_FACTORY = new JavaType(
-      "org.springframework.roo.addon.web.mvc.exceptions.annotations.RooLinkFactory");
+      "org.springframework.roo.addon.web.mvc.thymeleaf.annotations.RooLinkFactory");
   public static final JavaType ROO_THYMELEAF = new JavaType(
       "org.springframework.roo.addon.web.mvc.thymeleaf.annotations.RooThymeleaf");
   public static final JavaType ROO_THYMELEAF_MAIN_CONTROLLER = new JavaType(
